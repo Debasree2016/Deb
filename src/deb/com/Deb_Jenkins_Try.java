7af9135fbd056b413e24a8b7d3e12acc5f138641
Debasree2016 <debaband@in.ibm.com>
2017-06-30 19:33:01 +0530

    Initial commit

---
+++ src/deb/com/Deb_Jenkins_Try.java
@@ -0,0 +1,12 @@
+package deb.com;
+
+public class Deb_Jenkins_Try {
+	
+	public static void main(String args[]){
+		
+		System.out.println("Kemon Acho Jenkins");
+		System.out.println("Dekhi Ki Hocche");
+		System.out.println("test");
+	}
+
+}
